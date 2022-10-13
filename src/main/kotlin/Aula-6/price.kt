e727546165cdbe7cfd62959a1838042f20688617
antonio-paulino <antonioppaulino2@gmail.com>
2022-10-13 17:32:32 +0100

    Update Price exercise v1.1

--- src/main/kotlin/Aula-6/price.kt
+++ src/main/kotlin/Aula-6/price.kt
@@ -3,9 +3,11 @@ import kotlin.math.*
 fun main() {
     println("Indique o preço")
     val price= readln().toDouble()
-   // val euro =
+    val euro: Int = (price%100).roundToInt()
+    val cents1 = ((price - euro)*100.0).roundToInt()/100.0
+    val centsfinal = (cents1*100).roundToInt()
     val cents= price*100
-    println("O preço indicado foi de $price euros que corresponde a $cents centimos")
+    println("O preço indicado foi de $euro euros e $centsfinal centimos que corresponde a $cents centimos")
     println("Indique o desconto a fazer")
     val dis= readln().toDouble()
     val final= ((price-price*(0.01*dis))*100.0).roundToInt() /100.0 
