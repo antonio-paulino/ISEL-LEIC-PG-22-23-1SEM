14711ee3efdeb1ba94b825bb3e8dee4b05fb6517
antonio-paulino <antonioppaulino2@gmail.com>
2022-10-18 18:17:40 +0100

    Initial commit

--- src/main/kotlin/Aula-2/Numeros.kt
+++ src/main/kotlin/Aula-2/Numeros.kt
@@ -1,15 +1,33 @@
+package `Aula-8`
 fun main() {
- println("Enter Two number")
- var (a, b) = readln().split(' ') 
- println("Max number is : ${maxNum(a.toInt(), b.toInt())}")
+    println("Enter Two number")
+    var (a, b) = readln().split(' ')
+    println("Max number is : ${maxNum(a.toInt(), b.toInt())}")
+
 }
+
+fun digitAToInt(a: Char, b: Char): Int {
+    if (a !in '0'..'1')
+        throw Exception("Esta char n é um algarismo")
+    else
+        return a - '0'
+}
+
+fun digitbToInt(a: Char, b: Char): Int {
+    if (b !in '0'..'1')
+        throw Exception("Esta char n é um algarismo")
+    else
+        return b - '0'
+}
+
 fun maxNum(a: Int, b: Int): Int {
- var max = if (a > b) {
- println("The value of a is $a");
- a
- } else {
- println("The value of b is $b");
- b
- }
- return max;
+    var max = if (a > b) {
+        println("The value of a is $a");
+        a
+    } else {
+        println("The value of b is $b");
+        b
+    }
+    return max;
 }
+
--- src/main/kotlin/Aula-8/stringgraph.kt
+++ src/main/kotlin/Aula-8/stringgraph.kt
@@ -9,13 +9,18 @@ fun main() {
     for(ch in str)
         println(ch)
 // ou
-for(i in 0..str.length -1)
+/*for(i in 0..str.length -1)
             println(str[i])
 
     val words: List<String> = str.split(" ")
 
     // list<string> = lista imutável
-
-
     println(words)
+    */
+}
+fun digitToInt(ch :Char): Int{
+    if(ch !in '0'..'1')
+        throw Exception ("Esta char n é um algarismo")
+    else
+        return ch - '0'
 }
