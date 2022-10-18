267caa132974beb0155338138576fd676edbe948
antonio-paulino <antonioppaulino2@gmail.com>
2022-10-18 18:25:35 +0100

    Initial commit

---
+++ src/main/kotlin/Aula-8/Teste1.kt
@@ -0,0 +1,13 @@
+package `Aula-8`
+fun main() {
+
+
+    val str = " hoje vai chover - 456 "
+    val c: Char = 'A' + (str [0] - 'a')
+    val x: Int = 'B'.code + 1
+    val ch: Char = 'x'
+    val i: Int = (str[18] - '0') + ( str [19] - '0') + ( str [20] - '0')
+    println(" c = $c ")
+    println(" ch = $ch ")
+    println(" i = $i ")
+}
